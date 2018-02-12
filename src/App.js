55c7accef24f418a7fb659a9347707921e18ae21
ferryal <Ferryal21@gmail.com>
2018-02-12 09:55:38 +0700

    :hammer: using async and await into app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import Title from './components/Title'
 import Form from './components/Form'
+import Title from './components/Title'
+import Weather from './components/Weather'
 
+const API_KEY ="95611c5aee12a6c0aa524eb79dffa7bf"
 
-const API_KEY= "95611c5aee12a6c0aa524eb79dffa7bf";
 class App extends Component {
+  getWeather = async (e)  => {
+    e.preventDefault();
+    const api_call = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=Semarang,ID&appid=${API_KEY}&units=metric`)
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div>
         <Title />
-        <Form />
+        <Form getWeather={this.getWeather}/>
+        <Weather />
       </div>
     );
   }
